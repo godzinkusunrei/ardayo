7e33602a40de7d9bc713c420630a87dd9df81999
godzinkusunrei <aoandong83@gmail.com>
2022-11-30 18:25:25 +0900

    first commit

---
+++ arscript.js
@@ -0,0 +1,29 @@
+function mouseDown(){
+    var str = document.querySelector(".btn");
+
+    str.style.background="yellow";
+}
+
+function mouseUp(){
+    var str = document.querySelector(".btn");
+
+    str.style.background="#FFFFFF";
+}
+
+/*
+
+function mouseDownrotate(){
+    var str = document.querySelector(".btn2");
+    var obj = document.getElementById('Model_0');
+
+    obj.onclick;
+
+    str.style.background="yellow";
+}
+
+function mouseUprotate(){
+    var str = document.querySelector(".btn2");
+
+    str.style.background="#FFFFFF";
+}
+*/
